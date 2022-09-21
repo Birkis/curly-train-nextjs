f31bb623c51ae4935e2e83330282df3c1a15b4b4
Maximilian Øystå Lloyd <me@maxlloyd.no>
2022-09-21 22:25:10 +0200

    Portfoliosider ferdig

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,23 +1,39 @@
-import React from 'react'
-import styles from '../styles/navbar.module.css'
+import React, {useState} from 'react'
+import styles from '../styles/navbar.module.scss'
 import Image from 'next/image'
 import Link from 'next/link'
 
+
+
+
+
+
 export function Navbar() {
+
+    const [hideNav, setHideNav] = useState(true)
+
+
+
 
     return (
         <nav className={styles.navbar}>
         <div className={styles.logo}>
           <Link href="/"><Image src="/logo.png" width={286} height={126} /></Link>
         </div>
-        <div>
-          <ul className={`${styles.links} body-2`}>
-            <Link href="/"><li><a>Home </a></li></Link>
-            <Link href="/portfolio"><li><a>Portfolio </a></li></Link>
-            <Link href="/contact"><li><a>Contact Me </a></li></Link>
-            <Link href="/about"><li><a>About </a></li></Link>
-          
-          </ul>
+        <div className={styles.navWrapper}>
+          <button className={styles.mobileNavToggle}  onClick={()=> setHideNav(!hideNav)}>
+            <img className={`${styles.iconHamburger} ${hideNav ? "" : "visually-hidden"}`} src="/icons/hamburger.svg" alt="" />
+            <img  className={`${styles.iconclose} ${hideNav ? "visually-hidden" : ""}`} src="/icons/close.svg" alt="" />
+            <span className='visually-hidden'>menu</span>
+          </button>
+          <nav className={ hideNav ? `${styles.primaryNavigationHidden}` : `${styles.primaryNavigation}`   }>
+            <ul className={`${styles.links} body-2`} role='list'>
+              <Link href="/"><li><a>Home </a></li></Link>
+              <Link href="/portfolio"><li><a>Portfolio </a></li></Link>
+              <Link href="/contact"><li><a>Contact Me </a></li></Link>
+              <Link href="/about"><li><a>About </a></li></Link>          
+            </ul>
+          </nav>
         </div>
       </nav>  
     )
--- pages/portfolio/index.jsx
+++ pages/portfolio/index.jsx
@@ -2,12 +2,10 @@
 
 import { PortfolioItem } from "../../components";
 import  sanityClient  from "../../sanity";
+import styles from  '../../styles/portfolioItemPage.module.scss'
 
 export default function Portfolio({portfoliopages}) {
     
-  
-
-
     return (
       portfoliopages.map((item, index) => {
             
@@ -15,36 +13,28 @@ export default function Portfolio({portfoliopages}) {
             // console.log(pagenum)
 
             return (
-                <PortfolioItem key={item.id} pagenum={pagenum} item = {item}/>
+                <PortfolioItem classname={styles.PortfolioItem}  key={item.id} pagenum={pagenum} item = {item} index = {index}/>
                 
             )
         })
-
-
     )
 }
-
-
-  
 
 export const getServerSideProps =  async () => {
 
   const query = '*[ _type == "portfolioPage"]'
   const portfoliopages = await sanityClient.fetch(query)
 
-  
+  if(!portfoliopages.length) {
+    return {props: {
+      portfoliopages: ["hello", "byebye"]
+    }}
+  } else {
 
-if(!portfoliopages.length) {
-  return {props: {
-    portfoliopages: ["hello", "byebye"]
-  }}
-} else {
-
-  return {
-    props: {
-      portfoliopages
+    return {
+      props: {
+        portfoliopages
+      }
     }
   }
-}
-
 }
